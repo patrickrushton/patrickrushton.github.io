14f3f055c2b20773caec3df425979bd65b502586
Patrick Rushton <patrickrushton@gmail.com>
2021-05-10 13:39:52 +0200

    navigation adapted for mobile first

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", function () {
+
   new Glider(document.querySelector("#selected-work .carousel"), {
     slidesToShow: 1.1,
     slidesToScroll: 1,
     duration: 1,
     dots: "#selected-work .dots",
     arrows: {
-        prev: "#selected-work .glider-prev",
-        next: "#selected-work .glider-next",
+      prev: "#selected-work .glider-prev",
+      next: "#selected-work .glider-next",
     },
     responsive: [
       {
@@ -14,7 +15,7 @@ window.addEventListener("load", function () {
         settings: {
           slidesToShow: 2.2,
           slidesToScroll: 2,
-          },
+        },
       },
       {
         breakpoint: 900,
@@ -25,15 +26,16 @@ window.addEventListener("load", function () {
       },
     ],
   });
+
   new Glider(document.querySelector("#recommendations .carousel"), {
     slidesToShow: 1,
     slidesToScroll: 1,
     duration: 1,
     dots: "#recommendations .dots",
     arrows: {
-        prev: "#recommendations .glider-prev",
-        next: "#recommendations .glider-next",
-      },
+      prev: "#recommendations .glider-prev",
+      next: "#recommendations .glider-next",
+    },
     responsive: [
       {
         breakpoint: 700,
@@ -57,8 +59,9 @@ window.addEventListener("load", function () {
     duration: 1,
     dots: "#clients .dots",
     arrows: {
-        prev: "#clients .glider-prev",
-        next: "#clients .glider-next",
-      }
+      prev: "#clients .glider-prev",
+      next: "#clients .glider-next",
+    },
   });
+  
 }); //end load listener
