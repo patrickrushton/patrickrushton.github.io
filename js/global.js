8c9aece91d0dd915908a4e2e2ee64fd62164b88e
Patrick Rushton <patrickrushton@gmail.com>
2021-05-15 21:37:04 +0200

    completed goed bezig case

--- js/global.js
+++ js/global.js
@@ -1,39 +1,5 @@
-
-
+// Load includes
 $(function () {
   $("nav").load("includes/nav.html");
   $("#more-work").load("includes/more-work.html");
 });
-
-// $(document).ready(function () {
-//     $(".work-carousel").load("includes/work-carousel.html",
-//       function () {
-//         new Glider(document.querySelector("#selected-work .carousel"), {
-//           slidesToShow: 1.1,
-//           slidesToScroll: 1,
-//           duration: 1,
-//           dots: "#selected-work .dots",
-//           arrows: {
-//             prev: "#selected-work .glider-prev",
-//             next: "#selected-work .glider-next",
-//           },
-//           responsive: [
-//             {
-//               breakpoint: 700,
-//               settings: {
-//                 slidesToShow: 2.2,
-//                 slidesToScroll: 2,
-//               },
-//             },
-//             {
-//               breakpoint: 900,
-//               settings: {
-//                 slidesToShow: 3.2,
-//                 slidesToScroll: 3,
-//               },
-//             },
-//           ],
-//         });
-//       });
-//   });
-  
