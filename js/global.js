38e48710336b378f0dbb3e16b3fb61a9f23d29ce
Patrick Rushton <patrickrushton@gmail.com>
2021-05-15 19:53:12 +0200

    fix: moved smoothscroll from global.js to index.js

--- js/global.js
+++ js/global.js
@@ -1,8 +1,4 @@
-$.getScript(
-  "https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js"
-).done(() => {
-  var scroll = new SmoothScroll('a[href*="#"]');
-});
+
 
 $(function () {
   $("nav").load("includes/nav.html");
