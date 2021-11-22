6df71dffcd037ec21fa7516fb5979a03732d863f
Patrick Rushton <patrickrushton@gmail.com>
2021-11-22 10:57:36 +0100

    Disabled SmoothScroll on home page

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-$.getScript(
-  "https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js"
-).done(() => {
-  var scroll = new SmoothScroll('a[href*="#"]');
-});
+// $.getScript(
+//   "https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js"
+// ).done(() => {
+//   var scroll = new SmoothScroll('a[href*="#"]');
+// });
   
 $(function(){
   $(".work-carousel").load("includes/work-carousel.html");
